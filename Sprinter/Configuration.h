1638aa4f6e1dccca89a035ba20db9b194331f59a
Joachim Glauche <webmaster@joaz.de>
2013-01-13 13:35:56 +0100

    removed all the temperature and stepper movement code

--- Sprinter/Configuration.h
+++ Sprinter/Configuration.h
@@ -1,6 +1,3 @@
-#ifndef CONFIGURATION_H
-#define CONFIGURATION_H
-
 //// The following define selects which electronics board you have. Please choose the one that matches your setup
 // Teensy 2.0 driving lemon curry = 10
 #define MOTHERBOARD 10 
